6bf00b365ff93310348c470299d66199fff13af5
Supriyo Roy <roysupriyo10@gmail.com>
2023-09-05 13:20:03 +0530

    new config state

--- nvim/lua/roysupriyo10/lazy.lua
+++ nvim/lua/roysupriyo10/lazy.lua
@@ -91,8 +91,8 @@ local plugins = {
 				options = {
 					icons_enabled = true,
 					theme = 'auto',
-					component_separators = { left = '', right = ''},
-					section_separators = { left = '', right = ''},
+          component_separators = { left = '|', right = '|'},
+          section_separators = { left = '', right = ''},
 					disabled_filetypes = {
 						statusline = {},
 						winbar = {},
@@ -110,7 +110,7 @@ local plugins = {
 					lualine_a = {'mode'},
 					lualine_b = {'branch', 'diff', 'diagnostics'},
 					lualine_c = {'filename'},
-					lualine_x = {'encoding', 'fileformat', 'filetype'},
+					lualine_x = {'encoding', 'filetype'},
 					lualine_y = {'progress'},
 					lualine_z = {'location'}
 				},
