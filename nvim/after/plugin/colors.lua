a509f97c4e8c3212be463dc117025d0b660fb300
Supriyo Roy <roysupriyo10@gmail.com>
2024-02-12 23:20:43 +0530

    added tmux to xdg config home and created configuration

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,6 +1,7 @@
 function ColorSlate(color)
-	color = color or "rose-pine"
-	
+	color = color or "catppuccin"
+	-- color = color or "rose-pine"
+
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
