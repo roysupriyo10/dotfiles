896a9e274c12347082e3003af7912a36a150e9ce
Supriyo Roy <supriyo.roy@plutonapps.com>
2024-02-16 16:13:05 +0530

    added local scripts file to path and also updated nvim deps

--- nvim/lua/roysupriyo10/lazy.lua
+++ nvim/lua/roysupriyo10/lazy.lua
@@ -21,10 +21,9 @@ local plugins = {
 		branch = '0.1.x',
 		dependencies = {
 			'nvim-lua/plenary.nvim',
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = 'make' },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = 'make' },
 			'nvim-tree/nvim-web-devicons'
 		},
-
 	},
   {
     'catppuccin/nvim',
