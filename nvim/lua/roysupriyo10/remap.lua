139396fa335ef6ed34ed77dcd3859e1daec70141
Supriyo Roy <roysupriyo10@gmail.com>
2024-02-23 03:32:30 +0530

    added scripts, updated nvim lock file etc

--- nvim/lua/roysupriyo10/remap.lua
+++ nvim/lua/roysupriyo10/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set('n', "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set('n', ";", ":")
+-- vim.keymap.set('n', ";", ":")
 
 vim.keymap.set('n', "<leader>gp", ":silent %!prettier --stdin-filepath % <CR>")
 
