ee5b4967899ea57e6e82cd6fc4ad6c5d36875426
Supriyo Roy <roysupriyo10@gmail.com>
2024-02-25 03:17:49 +0530

    added new plugins for colorizing tailwind in nvim

--- nvim/lua/roysupriyo10/lazy.lua
+++ nvim/lua/roysupriyo10/lazy.lua
@@ -8,176 +8,185 @@ if not vim.loop.fs_stat(lazypath) then
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath, })
-end
-vim.opt.rtp:prepend(lazypath)
+  end
+  vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = ' '
+  vim.g.mapleader = ' '
 
--- list of plufins
-local plugins = {
-	{
-		'nvim-telescope/telescope.nvim',
-		-- tag = '0.1.2',
-		branch = '0.1.x',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = 'make' },
-			'nvim-tree/nvim-web-devicons'
-		},
+  -- list of plufins
+  local plugins = {
+    {
+      'nvim-telescope/telescope.nvim',
+      -- tag = '0.1.2',
+      branch = '0.1.x',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        { "nvim-telescope/telescope-fzf-native.nvim", build = 'make' },
+        'nvim-tree/nvim-web-devicons'
+      },
 
-	},
-  {
-    'catppuccin/nvim',
-    name = "catppuccin",
-    priority = 1000
-  },
-	{
-		'rose-pine/neovim',
-	},
-	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate'
-	},
-	'mbbill/undotree',
-	'tpope/vim-fugitive',
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets"
-		},
-		config = function()
-
-			local cmp = require("cmp")
-
-			local luasnip = require("luasnip")
-
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			cmp.setup({
-				completion = {
-					completeopt = "menu,menuone,preview,noselect",
-				},
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end
-				},
-
-				mapping = cmp.mapping.preset.insert({
-					['<C-k>'] = cmp.mapping.select_prev_item(),
-					['<C-j>'] = cmp.mapping.select_next_item(),
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					['<C-y>'] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' },
-          { name = 'codeium' },
-					{ name = 'buffer' },
-					{ name = 'path' },
-				}),
-			})
-		end
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
-		config = function()
-			require('lualine').setup {
-				options = {
-					icons_enabled = true,
-					theme = 'auto',
-          component_separators = { left = '|', right = '|'},
-          section_separators = { left = '', right = ''},
-					disabled_filetypes = {
-						statusline = {},
-						winbar = {},
-					},
-					ignore_focus = {},
-					always_divide_middle = true,
-					globalstatus = false,
-					refresh = {
-						statusline = 1000,
-						tabline = 1000,
-						winbar = 1000,
-					}
-				},
-				sections = {
-					lualine_a = {'mode'},
-					lualine_b = {'branch', 'diff', 'diagnostics'},
-					lualine_c = {'filename'},
-					lualine_x = {'encoding', 'filetype'},
-					lualine_y = {'progress'},
-					lualine_z = {'location'}
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = {'filename'},
-					lualine_x = {'location'},
-					lualine_y = {},
-					lualine_z = {}
-				},
-				tabline = {},
-				winbar = {},
-				inactive_winbar = {},
-				extensions = {}
-			}
-		end
-  },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 750
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  },
-  {
-    "terrortylor/nvim-comment",
-    event = "VeryLazy",
-    config = function ()
-      require('nvim_comment').setup({
-        comment_empty = true,
-        live_mapping = "gcc",
-        operator_mapping = "gc",
-      })
-    end
-  },
-  {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
     },
-    config = function()
-      require("codeium").setup({})
-    end
-  },
-  {
-    "github/copilot.vim"
-  }
-}
+    {
+      'catppuccin/nvim',
+      name = "catppuccin",
+      priority = 1000
+    },
+    {
+      'rose-pine/neovim',
+    },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      build = ':TSUpdate'
+    },
+    'mbbill/undotree',
+    'tpope/vim-fugitive',
+    {
+      "hrsh7th/nvim-cmp",
+      event = "InsertEnter",
+      dependencies = {
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets"
+      },
+      config = function()
+        local cmp = require("cmp")
 
-require('lazy').setup({plugins, { import = "roysupriyo10.lsp" }}, {
-	checker = {
-		enabled = true,
-		notify = false
-	},
-	change_detection = {
-		notify = false
-	}
-})
+        local luasnip = require("luasnip")
+
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        cmp.setup({
+          completion = {
+            completeopt = "menu,menuone,preview,noselect",
+          },
+          snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end
+          },
+
+          mapping = cmp.mapping.preset.insert({
+            ['<C-k>'] = cmp.mapping.select_prev_item(),
+            ['<C-j>'] = cmp.mapping.select_next_item(),
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+          }),
+          sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+            { name = 'codeium' },
+            { name = 'buffer' },
+            { name = 'path' },
+          }),
+        })
+      end
+    },
+    {
+      "nvim-lualine/lualine.nvim",
+      dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
+      config = function()
+        require('lualine').setup {
+          options = {
+            icons_enabled = true,
+            theme = 'auto',
+            component_separators = { left = '|', right = '|'},
+            section_separators = { left = '', right = ''},
+            disabled_filetypes = {
+              statusline = {},
+              winbar = {},
+            },
+            ignore_focus = {},
+            always_divide_middle = true,
+            globalstatus = false,
+            refresh = {
+              statusline = 1000,
+              tabline = 1000,
+              winbar = 1000,
+            }
+          },
+          sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', 'diagnostics'},
+            lualine_c = {'filename'},
+            lualine_x = {'encoding', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+          },
+          inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+          },
+          tabline = {},
+          winbar = {},
+          inactive_winbar = {},
+          extensions = {}
+        }
+      end
+    },
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 750
+      end,
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    },
+    {
+      "terrortylor/nvim-comment",
+      event = "VeryLazy",
+      config = function ()
+        require('nvim_comment').setup({
+          comment_empty = true,
+          live_mapping = "gcc",
+          operator_mapping = "gc",
+        })
+      end
+    },
+    {
+      "Exafunction/codeium.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+      },
+      config = function()
+        require("codeium").setup({})
+      end
+    },
+    {
+      "github/copilot.vim"
+    },
+    {
+      'NvChad/nvim-colorizer.lua',
+      config = function()
+        require('colorizer').setup({
+          user_default_options = {
+            tailwind = true
+          }
+        })
+      end
+    },
+  }
+
+  require('lazy').setup({plugins, { import = "roysupriyo10.lsp" }}, {
+    checker = {
+      enabled = true,
+      notify = false
+    },
+    change_detection = {
+      notify = false
+    }
+  })
