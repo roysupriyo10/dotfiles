63136864bc6f5f64ccf21feac7eab1da6326fb94
Supriyo Roy <roysupriyo10@gmail.com>
2024-02-29 00:42:23 +0530

    latest config state

--- nvim/lua/roysupriyo10/lazy.lua
+++ nvim/lua/roysupriyo10/lazy.lua
@@ -75,6 +75,16 @@ local plugins = {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ['<C-l>'] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { 'i', 's' }),
+          ['<C-h>'] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { 'i', 's' }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
