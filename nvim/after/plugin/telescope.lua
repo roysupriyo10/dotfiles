ed8fcade8c01b10f0d14f9daed285d297b150496
Supriyo Roy <roysupriyo10@gmail.com>
2024-08-21 23:55:51 +0530

    added remaps from figr workspace

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -7,11 +7,31 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>rf', builtin.oldfiles, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pw', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end, {})
 
+vim.keymap.set('n', '<leader>pa', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pb', function()
+  builtin.buffers({ sort_lastused = true })
+end, {})
+vim.keymap.set('n', '<leader>ps', function()
+  builtin.live_grep({
+    additional_args = { "--sortr=modified" },
+  })
+end, {})
+vim.keymap.set('n', '<leader>fe', function()
+  local search = vim.fn.input("Extension > ");
+  builtin.find_files({
+    find_command = {
+      "fd",
+      ".",
+      "--extension",
+      search,
+      "--no-ignore",
+    }
+  })
+end, {})
 
 -- action remaps
 telescope.setup({
